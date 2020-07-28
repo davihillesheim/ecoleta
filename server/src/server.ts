2f99f9968791ff7c805773d507cf920375e81f12
Davi Hillesheim <davi-241@hotmail.com>
2020-07-28 13:26:44 -0300

    set up migrations

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('listagem de usuários');
-
-  response.json([
-    'Davi',
-    'John',
-    'Carl',
-  ]);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
