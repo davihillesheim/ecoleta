804e3df31e5c5d1e02e0c5457928d4713ca5d3f8
Davi Hillesheim <davi.hillesheim@grad.ufsc.br>
2020-08-28 19:31:53 -0300

    user is redirected to home page after creating point

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft} from 'react-icons/fi'
 import { Map, TileLayer, Marker} from 'react-leaflet';
 import { LeafletMouseEvent} from 'leaflet';
@@ -42,6 +42,8 @@ const CreatePoint = () => {
     email: '',
     whatsapp: '',
   });
+
+  const history = useHistory();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
@@ -131,6 +133,8 @@ const CreatePoint = () => {
     await api.post('points', data);
 
     alert('Ponto de coleta criado!');
+
+    history.push('/');
   }
 
   return (
